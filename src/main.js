6e485abf5b4af8112b7afec96b0814467e9ef27e
Michael Lange <dingoeatingfuzz@gmail.com>
2016-08-21 19:36:47 -0700

    Did more things

--- src/main.js
+++ src/main.js
@@ -2,19 +2,19 @@
 import {} from './styles/main.css'
 import {} from './index.html'
 
-document.addEventListener('DOMContentLoaded', () => {
-  setNavbarSize()
-});
+// document.addEventListener('DOMContentLoaded', () => {
+//   setNavbarSize()
+// });
 
-window.addEventListener('resize', () => {
-  window.requestAnimationFrame(() => {
-    setNavbarSize()
-  })
-})
+// window.addEventListener('resize', () => {
+//   window.requestAnimationFrame(() => {
+//     setNavbarSize()
+//   })
+// })
 
-function setNavbarSize() {
-  document.querySelector('.fancy-nav').setAttribute(
-    'height',
-    Math.min(Math.max(window.innerWidth / 10, 80), 150) + 'px'
-  )
-}
+// function setNavbarSize() {
+//   document.querySelector('.fancy-nav').setAttribute(
+//     'height',
+//     Math.min(Math.max(window.innerWidth / 10, 80), 150) + 'px'
+//   )
+// }
